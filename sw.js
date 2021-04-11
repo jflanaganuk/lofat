f8ef4bff43b7d30aba11c54e86f798cf89ce983a
Joshua Flanagan <josh_flanagan@live.co.uk>
2021-04-11 22:56:28 +0100

    CSS fix for actor on small screen

--- sw.js
+++ sw.js
@@ -14,8 +14,8 @@
 // Names of the two caches used in this version of the service worker.
 // Change to v2, etc. when you update any of the local resources, which will
 // in turn trigger the install event again.
-const PRECACHE = 'precache-v1.0.40';
-const RUNTIME = 'runtime-v1.0.40';
+const PRECACHE = 'precache-v1.0.41';
+const RUNTIME = 'runtime-v1.0.41';
 
 const offlineUrl = "./offline";
 
