df32c3a46d804554f52d7e6d8bc25b941237f430
Joshua Flanagan <josh_flanagan@live.co.uk>
2021-04-14 16:53:07 +0100

    Improved local dev

---
+++ serve.ts
@@ -0,0 +1,18 @@
+import express from 'express';
+const app = express();
+
+app.use('/public', express.static(__dirname + '/public'));
+app.use('/imdbfetch', express.static(__dirname + '/'));
+app.use('/imdbfetch/*', express.static(__dirname + '/index.html'));
+app.use('/', express.static(__dirname + '/'));
+
+app.get('/', (req, res) => {
+    res.sendFile(`${__dirname}/index.html`)
+})
+
+app.listen(3000, () => {
+    console.log("====-====[Listening on port 3000]====-====");
+    setTimeout(() => {
+        console.log("Visit the site at : http://localhost:3000/imdbfetch")
+    }, 5000);
+})
