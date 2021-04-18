1e755017de7929bf25c9c8eeddb82a2afa2fcc89
Joshua Flanagan <josh@wearejh.com>
2021-04-19 00:05:58 +0100

    Added multi trailers

--- src/trailer.tsx
+++ src/trailer.tsx
@@ -1,19 +1,39 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { TmdbMovieVideos } from "../types";
 
 import "./trailer.scss";
 
 export const Trailer = (props: { trailer: TmdbMovieVideos | null }) => {
     if (!props.trailer) return null;
+    if (!props.trailer.results) return null;
+    const [currentTrailer, setCurrentTrailer] = useState(0);
+    useEffect(() => {
+        setCurrentTrailer(0);
+    }, [props.trailer.results])
     return (
         <div className="trailer">
             {props.trailer.results.length > 0 && (
                 <iframe
-                    title={`Trailer for ${props.trailer.results[0].name} film`}
+                    title={`Trailer for ${props.trailer.results[currentTrailer]?.name} film`}
                     className="trailerFrame"
-                    src={`https://www.youtube.com/embed/${props.trailer.results[0].key}`}
+                    src={`https://www.youtube.com/embed/${props.trailer.results[currentTrailer]?.key}`}
                     allowFullScreen
                 />
+            )}
+            {props.trailer.results.length > 1 && (
+                <div className="trailerButtonContainer">
+                    {props.trailer.results.map((result, index) => {
+                        return (
+                            <button
+                                className="trailerButton"
+                                key={result.id}
+                                onClick={() => setCurrentTrailer(index)}
+                            >
+                                {result.name} - {result.type}
+                            </button>
+                        );
+                    })}
+                </div>
             )}
         </div>
     );
