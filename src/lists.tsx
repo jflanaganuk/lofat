c3e040e76c95cc5b54023c9387d58938ecfc48d8
Joshua Flanagan <josh_flanagan@live.co.uk>
2021-05-09 15:48:15 +0100

    Added multiple lists for movies and tv

--- src/lists.tsx
+++ src/lists.tsx
@@ -1,15 +1,33 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import "./home.scss";
+import "./lists.scss";
 
 export const Lists = () => {
     return (
-        <div className="homeContainer">
-            <div className="homeList">
-                <Link to="/movie/">Popular Movies</Link>
-                <Link to="/tv/">Popular TV Shows</Link>
-                <Link to="/actors/">Popular Actors</Link>
-                <Link to="/lists/">More...</Link>
+        <div className="listContainer">
+            <div className="listInner">
+                <div className="listInnerSection">
+                    <h2>Movies</h2>
+                    <Link to="/movie/?list=popular">Popular Movies</Link>
+                    <Link to="/movie/?list=top_rated">Top Rated Movies</Link>
+                    <Link to="/movie/?list=now_playing">
+                        Now Playing Movies
+                    </Link>
+                    <Link to="/movie/?list=upcoming">Upcoming Movies</Link>
+                </div>
+                <div className="listInnerSection">
+                    <h2>TV</h2>
+                    <Link to="/tv/?list=popular">Popular TV Shows</Link>
+                    <Link to="/tv/?list=top_rated">Top Rated TV Shows</Link>
+                    <Link to="/tv/?list=airing_today">
+                        Airing Today TV Shows
+                    </Link>
+                    <Link to="/tv/?list=on_the_air">On The Air TV Shows</Link>
+                </div>
+                <div className="listInnerSection">
+                    <h2>Actors</h2>
+                    <Link to="/actors/">Popular Actors</Link>
+                </div>
             </div>
         </div>
     );
